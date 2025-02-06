874c9581b480c4ae67c9fd3b580b385b82a08067
Berkan <fanatik_berkan@hotmail.com>
2025-02-06 20:05:20 +0300

    commit yapıldı

---
+++ pages/shop-page.vue
@@ -0,0 +1,20 @@
+<template>
+    <NavBar />
+    <SingleProduct />
+    <Footer />
+</template>
+
+<script>
+import NavBar from "~/components/nav-bar.vue";
+import SingleProduct from "~/components/singleProduct.vue";
+import Footer from "~/components/footer.vue";
+
+export default {
+    components: {
+        NavBar,
+        SingleProduct,
+        Footer,
+    },
+};
+
+</script>
