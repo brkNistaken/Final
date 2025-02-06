cc6a8ea93db3945654ca83e9c27a6b1f0eba3c5c
Berkan <fanatik_berkan@hotmail.com>
2025-02-06 20:04:20 +0300

    commit yapıldı

---
+++ pages/MemberPage.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <div class="page-container">
+        <Navbar />
+        <MemberPage />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Navbar from "~/components/nav-bar.vue";
+import Footer from "~/components/Footer.vue";
+import MemberPage from "~/components/member.vue";
+
+export default {
+  components: {
+    Navbar,
+    Footer,
+    MemberPage,
+  },
+};
+</script>
