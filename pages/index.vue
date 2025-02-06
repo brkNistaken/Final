5f9702f7f885ad0461dec702cab171e4401fe643
Berkan <fanatik_berkan@hotmail.com>
2025-02-06 20:04:03 +0300

    commit yapıldı

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <NavBar />
+    <FirstComp />
+    <ThirdComp />
+    <FourthComp />
+    <LastComp />
+
+    <Footer />
+    <TopButton />
+  </div>
+</template>
+
+<script>
+import FirstComp from "~/components/first-comp.vue";
+import ThirdComp from "~/components/third-comp.vue";
+import FourthComp from "~/components/fourth-comp.vue";
+import LastComp from "~/components/last-comp.vue";
+import NavBar from "~/components/nav-bar.vue";
+import Footer from "~/components/footer.vue";
+import TopButton from "~/components/TopButton.vue";
+
+export default {
+  components: {
+    FirstComp,
+    ThirdComp,
+    FourthComp,
+    LastComp,
+    NavBar,
+    Footer,
+    TopButton,
+  },
+};
+</script>
+
+<style scoped>
+/* Ana sayfa için özel stiller burada olabilir */
+</style>
