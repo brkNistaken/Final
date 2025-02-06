3b98f54ac7774682cc35048c1a5eff0b7c2a8ffb
Berkan <fanatik_berkan@hotmail.com>
2025-02-06 20:03:47 +0300

    commit yapıldı

---
+++ pages/gallery.vue
@@ -0,0 +1,64 @@
+<template>
+  <NavBar />
+  <div class="products-page">
+    <!-- Filter Section -->
+    <Gallery />
+
+    <!-- Products Section -->
+    <div class="products-grid">
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <!-- Add more products as needed -->
+    </div>
+  </div>
+  <TopButton />
+
+  <Footer />
+</template>
+
+<script>
+import Footer from "~/components/footer.vue";
+import Gallery from "~/components/Gallery.vue";
+import NavBar from "~/components/nav-bar.vue";
+import Product from "~/components/product.vue";
+import TopButton from "~/components/TopButton.vue";
+
+export default {
+  components: {
+    Gallery,
+    Product,
+    NavBar,
+    Footer,
+    TopButton,
+  },
+};
+</script>
+
+<style scoped>
+.products-page {
+  display: flex;
+  padding: 20px;
+}
+
+.products-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 20px;
+  margin-left: 20px;
+  flex-grow: 1;
+}
+</style>
