e6b8eb5fa1b4b3bc637c759f69cc0c1197a0ab13
Berkan <fanatik_berkan@hotmail.com>
2025-02-06 20:00:39 +0300

    commit yapıldı

---
+++ pages/arama.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <Search @search="handleSearch" />
+    </div>
+  </template>
+  
+  <script>
+  import Search from "../components/search.vue";
+  
+  export default {
+    components: {
+      Search,
+    },
+    methods: {
+      handleSearch({ query, filters }) {
+        console.log("Arama Sorgusu:", query);
+        console.log("Filtreler:", filters);
+        // Arama işlemini burada gerçekleştirin
+      },
+    },
+  };
+  </script>
