0e9649e95588f77eb12f28d7487b9b44273b5ec3
Berkan <fanatik_berkan@hotmail.com>
2025-02-06 20:02:10 +0300

    commit yapıldı

---
+++ pages/bag-shopping.vue
@@ -0,0 +1,20 @@
+<template>
+    <NavBar />
+    <BagShopping />
+    <Footer />
+</template>
+
+<script>
+import NavBar from "~/components/nav-bar.vue";
+import Footer from "~/components/footer.vue";
+import BagShopping from "~/components/shop-bag.vue";
+
+export default {
+    components: {
+        NavBar,
+        BagShopping,
+        Footer,
+    },
+};
+
+</script>
