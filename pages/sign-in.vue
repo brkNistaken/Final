cf641f411faa016f71429273c378e8658e5d4dac
Berkan <fanatik_berkan@hotmail.com>
2025-02-06 20:05:48 +0300

    commit yapıldı

---
+++ pages/sign-in.vue
@@ -0,0 +1,20 @@
+<template>
+    <NavBar />
+    <SignIn />
+    <Footer />
+</template>
+
+<script>
+import NavBar from "~/components/nav-bar.vue";
+import SignIn from "~/components/Sign-In.vue";
+import Footer from "~/components/footer.vue";
+
+export default {
+    components: {
+        NavBar,
+        SignIn,
+        Footer,
+    },
+};
+
+</script>
